3a30a40bf21a78d44244409181f0031f03f9e40a
Angelo Tassi <115227916+Angelo-Tassi@users.noreply.github.com>
2025-02-11 17:25:26 +0100

    All collisions Working

--- tappy/scenes/pipes/pipes.gd
+++ tappy/scenes/pipes/pipes.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 class_name Pipes
 
+const OFF_SCREEN: float = -500.0
+@onready var score_sound: AudioStreamPlayer = $ScoreSound
+
+
 func _ready() -> void:
 	SignalManager.on_plane_died.connect(on_plane_died)
+	SignalManager._on_pipe_body_entered.connect(_on_pipe_body_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,6 +28,16 @@ func on_plane_died() -> void:
 
 
 func _on_pipe_body_entered(body: Node2D) -> void:
-	if body.is_in_group(GameManager.GROUP_PLANE) == true:
-		if body.has_method ("die") == true:
-			body.die
+	#TWO METHODS
+	#USING CLASS_NAME PLANE ON THE PLANE SCENE TO DEFINE THE CLASS
+	if body is plane:
+		body.die()
+	#USING GLOBAL GROUP TO DEFINE THE PLANE(PRE-ASSIGNED to GROUP)
+	#if body.is_in_group(GameManager.GROUP_PLANE) == true:
+		#if body.has_method ("die") == true:
+			#body.die()
+
+
+func _on_laser_body_entered(body: Node2D) -> void:
+	if body is plane:
+		score_sound.play()
--- tappy/sigletons/signal_manager.gd
+++ tappy/sigletons/signal_manager.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal  on_plane_died
+signal _on_pipe_body_entered
